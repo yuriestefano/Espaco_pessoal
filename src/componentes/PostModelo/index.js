2a45299a19b5f32f156eb0f60a2f145d45ed3931
yuriestefano <yuriestefano155@gmail.com>
2024-02-23 17:04:24 -0300

    .

---
+++ src/componentes/PostModelo/index.js
@@ -0,0 +1,23 @@
+import { Children } from "react"
+import styles from "./PostModelo.module.css"
+
+export default function PostModelo({children,fotoCapa,titulo}){
+    return (
+        <article className={styles.PostModeloContainer}>
+                <div
+                    className={styles.fotoCapa}
+                    style={{backgroundImage:`url(${fotoCapa})`}}
+
+                ></div>
+                  <h2 className={styles.titulo}>
+                    {titulo}
+
+                </h2>
+              
+                <div className={styles.PostConteudoContainer}>
+                {children}
+
+                </div>
+        </article>
+    )
+}
--- src/paginas/sobreMim/index.js
+++ src/paginas/sobreMim/index.js
@@ -1,5 +1,31 @@
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png"
+import styles from './sobreMim.module.css'
+import fotoSobremim from "assets/sobre_mim_foto.png"
+
+
 export default function SobreMim(){
     return(
-        <h1>Sobre Mim</h1>
+        
+        <PostModelo fotoCapa={fotoCapa} 
+        titulo={"Sobre Mim"}
+        
+        
+        >
+            <h3 className={styles.subtitulo}>
+                Olá, Eu sou Yuri
+
+            </h3>
+
+            <img 
+            src={fotoSobremim}
+            alt
+            
+            />
+            
+           
+
+            
+        </PostModelo>
     )
 }
